9ea3ae69a3ec313992504512b3d034db62eedf9e
Sir Nightmare <ivantochilkin@gmail.com>
2016-09-24 20:01:51 +0300

    the task is done

--- README.md
+++ README.md
@@ -6,11 +6,13 @@ https://dazzlepod.com/site_media/txt/passwords.txt
 https://forum.antichat.ru/threads/281655/
 
 password requirements:
-the use of both upper-case and lower-case letters (case sensitivity)
-inclusion of one or more numerical digits
-inclusion of special characters, such as @, #, $
-prohibition of words found in a password blacklist
-prohibition of words found in the user's personal information
-prohibition of use of company name or an abbreviation
-prohibition of passwords that match the format of calendar dates, license plate numbers,
-telephone numbers, or other common numbers
+
+inclusion at least 5 characters
+5 <= n <= 7: +0
+8 <= n <= 10 +1
+11 <= n <= 14 +2
+n => 15: +3
+the use of both upper-case and lower-case letters (case sensitivity) +2
+inclusion of one or more numerical digits +2
+inclusion of special characters, such as @, #, $ +2
+prohibition of words found in a password blacklist - score = 1
--- password_strength.py
+++ password_strength.py
@@ -1,22 +1,62 @@
+import re
+
+
 def load_data(filepath):
     data = ""
     with open(filepath, 'r', encoding="utf8") as file:
-        data = file.read()
+        data = file.read().split()
     return data
 
 
 def get_password_strength(password, blacklist):
-    if password in blacklist:
-        return 'Score: 1. Your password was found in blacklist'
+    if password in blacklist:  # check blacklist
+        return 0
+    score = 1
 
+    if len(password) < 5:  # check password length +3
+        return -2
+    elif 8 <= len(password) <= 10:
+        score += 1
+    elif 11 <= len(password) <= 14:
+        score += 2
+    else:
+        score += 3
 
+    if re.search('[А-Яа-яA-Za-z]', password):  # check letters +1
+        score += 1
+    else:
+        return -1
 
+    if re.search('\d', password):  # check digits +1
+        score += 1
+    if re.search('\W', password):  # check special characters +2
+        score += 2
+    if not (password.isupper() or password.islower()):  # check upper and lowercase +2
+        score += 2
+    return score
 
 
 if __name__ == '__main__':
-    list_of_passwords = 'blacklist.txt'
-    if input('Do you want to use your own password list? (y/n)').lower() == 'y':
-        list_of_passwords = input('Type path to the file:\n')
-    password_list = load_data(list_of_passwords)
+
+    filepath = 'blacklist.txt'
+    if input('Do you want to use your own password list? (y/n)\n').lower() == 'y':
+        filepath = input('Type path to the file:\n')
+    password_list = load_data(filepath)
     password = input('Type your password:\n')
-    print(get_password_strength(password, password_list))
+
+    score = get_password_strength(password, password_list)
+
+    result = ''
+    if score == 10:
+        result = 'Score: 10. Your password is perfect!'
+    elif score == 0:
+        result = 'Score: 1. Your password was found in blacklist'
+    elif score == -1:
+        result = 'Score: 1. Your password have to include at least one letter '
+    elif score == -2:
+        result = 'Score: 1. Your password have to include at least 5 characters'
+    elif 0 < score < 6:
+        result = 'Score: ' + str(score) + '. Your password is not strong enough. You should choose another one'
+    else:
+        result = 'Score: ' + str(score) + '. Your password is quite good :)'
+    print(result)
